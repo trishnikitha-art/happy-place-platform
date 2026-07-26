d303e66dd69f49f01a822a830fe5b374472b807d
trishnikitha-art <trishnikitha@gmail.com>
2026-07-26 15:47:05 -0600

    Fix footer tagline and disable Lenis for native scroll test
    1. Fixed footer tagline duplication:
       - Updated company.v1.json description to match new version
       - Old: 'Building decks, fences, remodels, repairs, custom carpentry...'
       - New: 'Decks, fences, repairs, remodels, finish carpentry, painting...'
       - This resolves the two different taglines appearing in footer
    
    2. Disabled LenisProvider for native scroll performance test:
       - Commented out LenisProvider in layout.tsx
       - This allows testing native scroll vs Lenis scroll
       - Will definitively separate code bug from Lenis design trade-off
       - Revert by uncommenting LenisProvider after test

--- website/src/app/layout.tsx
+++ website/src/app/layout.tsx
@@ -77,7 +77,8 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
       <body className="min-h-full flex flex-col bg-background">
         <ThemeProvider defaultTheme="system" storageKey="hpp-theme">
           <MotionProvider>
-            <LenisProvider>
+            {/* TEMPORARY: LenisProvider disabled for native scroll performance test */}
+            {/* <LenisProvider> */}
               <script
               dangerouslySetInnerHTML={{
                 __html: `
@@ -100,7 +101,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
             <SiteHeader />
             <main className="flex-1">{children}</main>
             <SiteFooter />
-            </LenisProvider>
+            {/* </LenisProvider> */}
           </MotionProvider>
         </ThemeProvider>
       </body>
