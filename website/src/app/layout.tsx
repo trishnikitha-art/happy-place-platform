c254936771394ef624b25336ded4f6784f5c095f
trishnikitha-art <trishnikitha@gmail.com>
2026-07-26 12:11:52 -0600

    Fix TypeScript error: wrap children in LenisProvider
    LenisProvider now requires children prop. Wrapped all layout children
    inside the provider to fix the build error.

--- website/src/app/layout.tsx
+++ website/src/app/layout.tsx
@@ -77,8 +77,8 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
       <body className="min-h-full flex flex-col bg-background">
         <ThemeProvider defaultTheme="system" storageKey="hpp-theme">
           <MotionProvider>
-            <LenisProvider />
-            <script
+            <LenisProvider>
+              <script
               dangerouslySetInnerHTML={{
                 __html: `
                   (function() {
@@ -100,6 +100,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
             <SiteHeader />
             <main className="flex-1">{children}</main>
             <SiteFooter />
+            </LenisProvider>
           </MotionProvider>
         </ThemeProvider>
       </body>
